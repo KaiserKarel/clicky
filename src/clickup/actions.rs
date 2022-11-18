3c1e18441274584b845215d7cf30872a2e1aafdd
cor <cor@pruijs.dev>
2022-11-18 13:30:32 +0100

    feat: set_task_parent

--- src/clickup/actions.rs
+++ src/clickup/actions.rs
@@ -11,6 +11,11 @@ struct CreateTaskParameters {
     pub parent: String,
 }
 
+#[derive(Serialize, Clone, Hash)]
+struct SetTaskParentParams<'a> {
+    pub parent: &'a TaskId,
+}
+
 const LIST_ID: ListId = ListId(188335476);
 
 /// Creates a clickup task
@@ -49,6 +54,27 @@ pub async fn get_task(token: &ClickupToken, id: &TaskId) -> reqwest::Result<Task
         .await
 }
 
+pub async fn set_task_parent(
+    token: &ClickupToken,
+    id: &TaskId,
+    new_parent: &TaskId,
+) -> reqwest::Result<String> {
+    let client = reqwest::Client::new();
+
+    let url = format!("https://api.clickup.com/api/v2/task/{}", id.0);
+
+    let params = SetTaskParentParams { parent: new_parent };
+
+    client
+        .put(url)
+        .header(reqwest::header::AUTHORIZATION, token.0)
+        .json(&params)
+        .send()
+        .await?
+        .text()
+        .await
+}
+
 // pub async fn set_task_parent(authorization: &str
 
 #[cfg(test)]
@@ -73,6 +99,19 @@ mod tests {
         let res = get_task(&CLICKUP_TOKEN, &TaskId::from("3vj469b"))
             .await
             .unwrap();
+        dbg!(res);
+    }
+
+    #[tokio::test]
+    #[traced_test]
+    async fn can_set_task_parent() {
+        let res = set_task_parent(
+            &CLICKUP_TOKEN,
+            &TaskId::from("3vj469b"),
+            &TaskId::from("36w74wp"),
+        )
+        .await
+        .unwrap();
         dbg!(res);
     }
 }
